e859a5faaa1b2ace2df34f4a2f04f5e3483ba7b8
maxileonardi <maxi_leonardi@hotmail.com>
2018-08-27 22:34:58 -0300

    Consultar datos de Cliente

--- thegym19-08/BussinessLayer/TheGym.cs
+++ thegym19-08/BussinessLayer/TheGym.cs
@@ -16,7 +16,16 @@ namespace thegym19_08.BusinessLayer
         public long TelefonoSucursal;
 
         public string NombreClienteBusc;
-        public string DNIClienteBusc="";
+        public string DNIClienteBusc="0";
+
+        public string DNIEditar="0";
+        public string NombreClienteEditar;
+        public string ApellidoClienteEditar;
+        public string TelefonoClienteEditar;
+        public string DomicilioClienteEditar;
+        public string FechaClienteEditar;
+        public string EmailClienteEditar;
+        public string DNIClienteEditar;
 
         public void AddNewSucursal()
         {
@@ -36,6 +45,19 @@ namespace thegym19_08.BusinessLayer
             return dt;
         }
 
+        public void UpdateCliente()
+        {
+            SqlParameter[] parameters = new SqlParameter[8];
+            parameters[0] = DataLayer.DataAccess.AddParameter("@Nombre", NombreClienteEditar, SqlDbType.NVarChar, 50);
+            parameters[1] = DataLayer.DataAccess.AddParameter("@Apellido", ApellidoClienteEditar, SqlDbType.NVarChar, 50);
+            parameters[2] = DataLayer.DataAccess.AddParameter("@Fecha_nac", FechaClienteEditar, SqlDbType.Date, 50);
+            parameters[3] = DataLayer.DataAccess.AddParameter("@Email", EmailClienteEditar, SqlDbType.NVarChar, 100);
+            parameters[4] = DataLayer.DataAccess.AddParameter("@Telefono", TelefonoClienteEditar, SqlDbType.BigInt, 50);
+            parameters[5] = DataLayer.DataAccess.AddParameter("@Domicilio", DomicilioClienteEditar, SqlDbType.NVarChar, 50);
+            parameters[6] = DataLayer.DataAccess.AddParameter("@DNI", DNIClienteEditar, SqlDbType.Int, 100);
+            parameters[7] = DataLayer.DataAccess.AddParameter("@DNIEdit", DNIEditar, SqlDbType.Int, 100);
+            DataTable dt = DataLayer.DataAccess.ExcecuteDTbyProcedure("PA_UpdateCliente", parameters);
+        }
 
 
     }
--- thegym19-08/Consultarclientes.aspx.cs
+++ thegym19-08/Consultarclientes.aspx.cs
@@ -11,13 +11,20 @@ namespace thegym19_08
 {
     public partial class Consultarclientes : System.Web.UI.Page
     {
+        static string DNIEdit;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            if (!Page.IsPostBack)
+            {
 
+            }
         }
 
         protected void btnbuscar_Click(object sender, EventArgs e)
         {
+            GridView1.Visible = true;
             if (CkbDNI.Checked)
             {
                 TheGym k = new TheGym();
@@ -42,9 +49,87 @@ namespace thegym19_08
                 }
             }
 
+   
 
         }
 
+        //protected void Button1_Click(object sender, EventArgs e)
+        //{
+        //    tbnombre.Text = GridView1
+        //}
 
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DNIEdit = GridView1.SelectedRow.Cells[2].Text;
+            tbnombre.ReadOnly = false;
+            tbapellido.ReadOnly = false;
+            tbdni.ReadOnly = false;
+            tbemail.ReadOnly = false;
+            tbfechanacimiento.ReadOnly = false;
+            tbtelefono.ReadOnly = false;
+            tbdomicilio.ReadOnly = false;
+            tbnombre.Text = GridView1.SelectedRow.Cells[0].Text;
+            tbapellido.Text = GridView1.SelectedRow.Cells[1].Text;
+            tbdni.Text = GridView1.SelectedRow.Cells[2].Text;
+            tbfechanacimiento.Text = GridView1.SelectedRow.Cells[3].Text;
+            tbemail.Text = GridView1.SelectedRow.Cells[4].Text;
+            tbtelefono.Text = GridView1.SelectedRow.Cells[5].Text;
+            //tbdomicilio.Text = GridView1.SelectedRow.Cells[7].Text;
+            if (GridView1.SelectedRow.Cells[6].Text == string.Empty)
+            {
+                tbdomicilio.Text = "";
+            }
+            else
+            {
+                tbdomicilio.Text = GridView1.SelectedRow.Cells[6].Text;
+            }
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+
+        }
+
+        protected void btneditar_Click(object sender, EventArgs e)
+        {
+            TheGym k = new TheGym
+            {
+                NombreClienteEditar = tbnombre.Text,
+                ApellidoClienteEditar = tbapellido.Text,
+                DNIClienteEditar = tbdni.Text,
+                FechaClienteEditar = tbfechanacimiento.Text,
+                EmailClienteEditar = tbemail.Text,
+                TelefonoClienteEditar = tbtelefono.Text,
+                DomicilioClienteEditar = tbdomicilio.Text
+            };
+
+            k.DNIEditar = DNIEdit;
+
+            k.UpdateCliente();
+
+            tbapellido.Text = string.Empty;
+            tbnombre.Text = string.Empty;
+            tbbuscar.Text = string.Empty;
+            tbemail.Text = string.Empty;
+            DNIEdit = string.Empty;
+            tbdni.Text = string.Empty;
+            tbfechanacimiento.Text = string.Empty;
+            tbtelefono.Text = string.Empty;
+            tbdomicilio.Text = string.Empty;
+
+            GridView1.Visible = false;
+            
+
+        }
     }
 }
